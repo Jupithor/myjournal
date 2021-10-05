bfc96d80b61e85e41d99fc4223d522e024ae4119
Thor <tryvard@gmail.com>
2021-10-05 09:44:51 +0200

    design changes

--- dagbog.sh
+++ dagbog.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/bash
+yesterday=$(date --date="-1 day" +"%A")
 
-echo "
-$(date +%d-%m-%Y)
-===============
-$1
-===============
-" >> dagbog.txt
+if [ $yesterday == "Sunday" ];
+then
+	yesterday=$(date --date="-3 days" +"%A %d-%m-%Y")
+else
+	yesterday=$(date --date="-1 day" +"%A %d-%m-%Y")
+	
+fi
+
+echo "$yesterday
+===================
+${1}
+
+$(cat dagbog.txt)" > dagbog.txt
